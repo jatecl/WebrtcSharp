476ff9697cd5ab1e9c803045ecd67f1a2771b44d
Jat Zhang <jatoanw@qq.com>
2019-12-10 18:57:49 +0800

    视频显示改用了OpenCV进行计算。

--- WebCamera/MainWindow.xaml.cs
+++ WebCamera/MainWindow.xaml.cs
@@ -24,81 +24,75 @@ namespace WebCamera
         private void OpenCamera()
         {
             source?.Release();
+            if (source != null) source.Frame -= Source_Frame;
             source = factory.CreateVideoSource(cameraIndex, 1600, 1200, 30);
             if (source == null) return;
             source.Frame += Source_Frame;
         }
 
+        protected override void OnClosed(EventArgs e)
+        {
+            source?.Release();
+            if (source != null) source.Frame -= Source_Frame;
+            base.OnClosed(e);
+        }
+
         [DllImport("gdi32.dll")]
         private extern static bool DeleteObject(IntPtr hObject);
 
+        /// <summary>
+        /// 计算YUV420视频帧占用的内存大小
+        /// </summary>
+        /// <param name="strideY">Y通道宽度</param>
+        /// <param name="width">视频宽度</param>
+        /// <param name="height">视频高度</param>
+        /// <returns></returns>
+        private int GetMemeryCount(int strideY, int width, int height)
+        {
+            int number = width / strideY;
+            if (number < 1) number = 1;
+            return strideY * height / number;
+        }
+
         private void Source_Frame(VideoFrame obj)
         {
-            var bitmap = YUV420ToBitmap(obj);
-            Dispatcher.BeginInvoke((Action)(() =>
+            var lenY = GetMemeryCount(obj.StrideY, obj.Width, obj.Height);
+            var lenU = GetMemeryCount(obj.StrideU, obj.Width, obj.Height);
+            var lenV = GetMemeryCount(obj.StrideV, obj.Width, obj.Height);
+            var lenA = 0;
+            if (obj.StrideA > 0) lenA = GetMemeryCount(obj.StrideA, obj.Width, obj.Height);
+
+            OpenCvSharp.Mat yuvImg = new OpenCvSharp.Mat();
+            yuvImg.Create((obj.Height * 3 / 2), obj.Width, OpenCvSharp.MatType.CV_8UC1);
+
+            unsafe
             {
-                try
+                Buffer.MemoryCopy(obj.DataY.ToPointer(), yuvImg.Data.ToPointer(), lenY, lenY);
+                Buffer.MemoryCopy(obj.DataU.ToPointer(), (byte*)yuvImg.Data.ToPointer() + lenY, lenU, lenU);
+                Buffer.MemoryCopy(obj.DataV.ToPointer(), (byte*)yuvImg.Data.ToPointer() + lenY + lenU, lenV, lenV);
+                if (lenA > 0) Buffer.MemoryCopy(obj.DataA.ToPointer(), (byte*)yuvImg.Data.ToPointer() + lenY + lenU + lenV, lenA, lenA);
+            }
+            using (var target = new OpenCvSharp.Mat())
+            {
+                OpenCvSharp.Cv2.CvtColor(yuvImg, target, OpenCvSharp.ColorConversionCodes.YUV2BGRA_I420);
+                using (var bitmap = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(target))
                 {
                     IntPtr myImagePtr = bitmap.GetHbitmap();
                     try
                     {
-                        var imgsource = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(myImagePtr, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());  //创建imgSource
-                        display.Source = imgsource;
+                        Dispatcher.Invoke(() =>
+                        {
+
+                            var imgsource = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(myImagePtr, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());  //创建imgSource
+                            display.Source = imgsource;
+                        });
                     }
                     finally
                     {
                         if (myImagePtr != IntPtr.Zero) DeleteObject(myImagePtr);
                     }
                 }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-                finally
-                {
-                    bitmap.Dispose();
-                }
-            }));
-        }
-        unsafe Bitmap YUV420ToBitmap(VideoFrame scaled)
-        {
-            int width = scaled.Width;
-            int height = scaled.Height;
-            byte* ydata = (byte*)scaled.DataY.ToPointer();
-            byte* udata = (byte*)scaled.DataU.ToPointer();
-            byte* vdata = (byte*)scaled.DataV.ToPointer();
-
-            int Y, U, V;
-
-            var bmp = new Bitmap(width, height);
-
-            BitmapData data = bmp.LockBits(new System.Drawing.Rectangle(0, 0, width, height), ImageLockMode.WriteOnly, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
-            byte* ptr = (byte*)data.Scan0.ToPointer();
-
-            for (var j = 0; j < height; ++j)
-            {
-                var sj = j * width;
-                var ssj = (j / 2) * (width / 2);
-                var start = sj * 3;
-                for (int i = 0; i < width; i++)
-                {
-                    var si = ssj + i / 2;
-                    Y = ydata[sj + i];
-                    U = udata[si];
-                    V = vdata[si];
-                    var index = i * 3;
-                    ptr[start + index + 2] = (byte)Math.Max(0, Math.Min(255, (int)Math.Round(Y + 1.4075 * (V - 128))));
-                    ptr[start + index + 1] = (byte)Math.Max(0, Math.Min(255, (int)Math.Round(Y - 0.3455 * (U - 128) - 0.7169 * (V - 128))));
-                    ptr[start + index + 0] = (byte)Math.Max(0, Math.Min(255, (int)Math.Round(Y + 1.779 * (U - 128))));
-                    if (j == height - 1 && i == width - 1)
-                    {
-                        break;
-                    }
-                }
             }
-            bmp.UnlockBits(data);
-
-            return bmp;
         }
 
         private VideoSource source;
